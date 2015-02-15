4f422baeaa7501b3cc9e462dfbd94fcba7dc8667
王梦杰 <aslinwang@wangmengjiedeMacBook-Pro.local>
2015-02-15 14:05:44 +0800

    support colorfull display and handle error

--- qn.js
+++ qn.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 'use strict'
-var config = require('./config/global.js');
+var colors = require('colors');
 
 var qn = {};
+var config = {};
 
+try{
+  config = require('./config/global.js');
+}
+catch(e){
+  console.log('[WARNING]  should first config global.js.'.red);
+  process.exit(1);
+}
 qn.config = config;
 
 module.exports = qn;
